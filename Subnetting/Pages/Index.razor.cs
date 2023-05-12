fa228edecdaafa459027bbbe5dbad69764629fdc
adrian <adrianlm1985@gmail.com>
2023-05-12 16:57:13 +0200

    Modificación contador y botón

--- Subnetting/Pages/Index.razor.cs
+++ Subnetting/Pages/Index.razor.cs
@@ -6,7 +6,10 @@ namespace Subnetting.Pages
         /*Eliminamos input al restar contador el for recorre uno menos y se ve uno menos*/
         private void Eliminar()
         {
-            cont--;
+            if(cont >= 1)
+            {
+                cont--;
+            }
         }
         /*Añadimos input al sumar contador el for recorre uno más y se ve uno más*/
         private void Contar()
