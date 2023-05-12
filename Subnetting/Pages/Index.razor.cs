448449dd6f84512d0fea56cd2512fe8f3863d8af
adrian <adrianlm1985@gmail.com>
2023-05-12 10:09:26 +0200

    Explicación y eliminación de otros intentos

---
+++ Subnetting/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+namespace Subnetting.Pages
+{
+    public partial class Index
+    {
+        private int cont;
+        /*Eliminamos input al restar contador el for recorre uno menos y se ve uno menos*/
+        private void Eliminar()
+        {
+            cont--;
+        }
+        /*Añadimos input al sumar contador el for recorre uno más y se ve uno más*/
+        private void Contar()
+        {
+            cont++;
+        }
+    }
+}
