84543f36fc57b9162249df6fba6fb6611d9e8737
Alejandro Palacios Martín <srjalean@gmail.com>
2023-05-12 18:03:47 +0200

    cambios

--- Subnetting/Pages/Index.razor.cs
+++ Subnetting/Pages/Index.razor.cs
@@ -2,11 +2,11 @@ namespace Subnetting.Pages
 {
     public partial class Index
     {
-        private int cont;
+        private int cont = 1;
         /*Eliminamos input al restar contador el for recorre uno menos y se ve uno menos*/
         private void Eliminar()
         {
-            if(cont >= 1)
+            if(cont > 1)
             {
                 cont--;
             }
